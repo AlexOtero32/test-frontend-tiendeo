af695f31f1b2b719951f151e7297321180b9879b
AlexOtero32 <alex.otero-94@hotmail.com>
2020-08-02 17:54:53 +0200

    Añadida test suite de Card

---
+++ src/__tests__/components/Card.spec.js
@@ -0,0 +1,121 @@
+import React from "react";
+import Card from "../../components/Card";
+import renderer from "react-test-renderer";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import { toggleModal } from "../../redux/ui";
+import { deleteCard } from "../../redux/cards/actions";
+
+const mockStore = configureStore([]);
+
+describe("Card component", function () {
+  let store;
+
+  let initialState = {
+    ui: {
+      isModalOpen: false,
+    },
+    cards: {
+      orderBy: "newest",
+      cardList: [
+        {
+          date: new Date(),
+          titulo: "Tarjeta",
+          id: "un-id-aleatorio",
+          descripcion: "Descripción",
+          url: undefined,
+        },
+      ],
+    },
+  };
+
+  beforeEach(() => {
+    store = mockStore(initialState);
+  });
+
+  it("Renderiza correctamente", function () {
+    const component = renderer.create(
+      <Provider store={store}>
+        <Card titulo="Titulo" descripcion="Descripción" />
+      </Provider>
+    );
+
+    let tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("El botón editar llama al dispatch de la store dos veces", function () {
+    store.dispatch = jest.fn();
+
+    // El botón editar llama a la función dispatch dos veces,
+    // una para seleccionar qué Card editar
+    // y otra para abrir el modal
+    const component = renderer.create(
+      <Provider store={store}>
+        <Card titulo="Titulo" descripcion="Descripción" />
+      </Provider>
+    );
+    renderer.act(() => {
+      component.root.findAllByType("button")[0].props.onClick();
+    });
+
+    expect(store.dispatch).toHaveBeenCalledTimes(2);
+  });
+
+  it("El botón eliminar llama al dispatch de la store una vez", function () {
+    store.dispatch = jest.fn();
+
+    // El botón editar llama a la función dispatch dos veces,
+    // una para seleccionar qué Card editar
+    // y otra para abrir el modal
+    const component = renderer.create(
+      <Provider store={store}>
+        <Card titulo="Titulo" descripcion="Descripción" />
+      </Provider>
+    );
+    renderer.act(() => {
+      component.root.findAllByType("button")[1].props.onClick();
+    });
+
+    expect(store.dispatch).toHaveBeenCalledTimes(1);
+  });
+
+  it("El botón editar abre el modal", function () {
+    store.dispatch = jest.fn();
+
+    // El botón editar llama a la función dispatch dos veces,
+    // una para seleccionar qué Card editar
+    // y otra para abrir el modal
+    const component = renderer.create(
+      <Provider store={store}>
+        <Card {...initialState.cards.cardList[0]} />
+      </Provider>
+    );
+    renderer.act(() => {
+      component.root.findAllByType("button")[0].props.onClick();
+    });
+
+    expect(store.dispatch).toHaveBeenLastCalledWith(toggleModal());
+  });
+
+  it("El botón eliminar elimnina la Card", function () {
+    store.dispatch = jest.fn();
+
+    // El botón editar llama a la función dispatch dos veces,
+    // una para seleccionar qué Card editar
+    // y otra para abrir el modal
+    const component = renderer.create(
+      <Provider store={store}>
+        <Card {...initialState.cards.cardList[0]} />
+      </Provider>
+    );
+    renderer.act(() => {
+      component.root.findAllByType("button")[1].props.onClick();
+    });
+
+    expect(store.dispatch).toHaveBeenLastCalledWith(
+      deleteCard("un-id-aleatorio")
+    );
+  });
+});
--- src/redux/cards/reducer.js
+++ src/redux/cards/reducer.js
@@ -1,80 +1,76 @@
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 import {
-    CREATE_CARD,
-    EDIT_CARD,
-    DELETE_CARD,
-    SET_EDITING_CARD,
-    CLEAR_EDITING_CARD,
-    ORDER_CARDS,
-} from './types';
-import { getStateFromLocalStorage, orderCardsBy } from '../../lib/helpers';
+  CREATE_CARD,
+  EDIT_CARD,
+  DELETE_CARD,
+  SET_EDITING_CARD,
+  CLEAR_EDITING_CARD,
+  ORDER_CARDS,
+} from "./types";
+import { getStateFromLocalStorage, orderCardsBy } from "../../lib/helpers";
 
 const stateFromLocalStorage = getStateFromLocalStorage();
 
 const initialState = {
-    cardList: stateFromLocalStorage ? stateFromLocalStorage.cardList : [],
-    editingCard: {},
-    orderBy: stateFromLocalStorage.orderBy
-        ? stateFromLocalStorage.orderBy
-        : 'newest',
+  cardList: stateFromLocalStorage ? stateFromLocalStorage.cardList : [],
+  editingCard: {},
+  orderBy: stateFromLocalStorage ? stateFromLocalStorage.orderBy : "newest",
 };
 
 export default (state = initialState, action) => {
-    let ordered = [];
-    switch (action.type) {
-        case CREATE_CARD:
-            const card = {
-                ...action.card,
-                id: uuid(),
-                fecha: new Date(),
-            };
+  let ordered = [];
+  switch (action.type) {
+    case CREATE_CARD:
+      const card = {
+        ...action.card,
+        id: uuid(),
+        fecha: new Date(),
+      };
 
-            ordered = orderCardsBy(state.orderBy, [...state.cardList, card]);
+      ordered = orderCardsBy(state.orderBy, [...state.cardList, card]);
 
-            return {
-                cardList: ordered,
-                editingCard: state.editingCard,
-                orderBy: state.orderBy,
-            };
+      return {
+        cardList: ordered,
+        editingCard: state.editingCard,
+        orderBy: state.orderBy,
+      };
 
-        case EDIT_CARD:
-            const cardList = state.cardList.map(function (card) {
-                if (card.id === action.card.id) {
-                    Object.assign(card, action.card);
-                }
-                return card;
-            });
+    case EDIT_CARD:
+      const cardList = state.cardList.map(function (card) {
+        if (card.id === action.card.id) {
+          Object.assign(card, action.card);
+        }
+        return card;
+      });
 
-            ordered = orderCardsBy(state.orderBy, cardList);
+      ordered = orderCardsBy(state.orderBy, cardList);
 
-            return {
-                ...state,
-                cardList: ordered,
-                editingCard: {},
-            };
+      return {
+        ...state,
+        cardList: ordered,
+        editingCard: {},
+      };
 
-        case DELETE_CARD:
-            return {
-                cardList: state.cardList.filter(
-                    (card) => card.id !== action.cardId
-                ),
-                editingCard: state.editingCard,
-            };
+    case DELETE_CARD:
+      return {
+        cardList: state.cardList.filter(card => card.id !== action.cardId),
+        editingCard: state.editingCard,
+      };
 
-        case SET_EDITING_CARD:
-            return { ...state, editingCard: action.card };
+    case SET_EDITING_CARD:
+      return { ...state, editingCard: action.card };
 
-        case CLEAR_EDITING_CARD:
-            return { ...state, editingCard: {} };
+    case CLEAR_EDITING_CARD:
+      return { ...state, editingCard: {} };
 
-        case ORDER_CARDS:
-            return {
-                ...state,
-                cardList: orderCardsBy(action.order, state.cardList),
-                orderBy: action.order,
-            };
+    case ORDER_CARDS:
+      return {
+        ...state,
+        cardList: orderCardsBy(action.order, state.cardList),
+        orderBy: action.order,
+      };
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
